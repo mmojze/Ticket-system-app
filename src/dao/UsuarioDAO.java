c1245d0c010d62287364c80d5b9d0844dd23a76c
mmojze <32619209+mmojze@users.noreply.github.com>
2021-10-29 22:08:07 -0300

    Ajustes DAO de Usuario

--- src/dao/UsuarioDAO.java
+++ src/dao/UsuarioDAO.java
@@ -1,10 +1,16 @@
 package dao;
 
+import java.util.List;
+
 import entidades.Usuario;
 import exceptions.DAOException;
 
 public interface UsuarioDAO {
 
-	public Usuario getUser(Usuario usuario) throws DAOException;
+	public Usuario consultarUsuario(Usuario usuario) throws DAOException;
+	public Usuario crearUsuario(Usuario usuario) throws DAOException;
+	public Usuario modificarUsuario(Usuario usuario) throws DAOException;
+	public void eliminarUsuario(Usuario usuario) throws DAOException;
+	public List<Usuario> listarUsuarios(Usuario usuario) throws DAOException;
 
 }
--- src/dao/UsuarioDAOH2.java
+++ src/dao/UsuarioDAOH2.java
@@ -1,19 +1,23 @@
 package dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import entidades.Espectaculo;
+import entidades.Ubicacion;
 import entidades.Usuario;
+import entidades.Venta;
 import exceptions.DAOException;
 import utilidades_db.DBManager;
 
 public class UsuarioDAOH2 implements UsuarioDAO {
 
-	public Usuario getUser(Usuario usuario) throws DAOException {
+	public Usuario consultarUsuario(Usuario usuario) throws DAOException {
 
 		Usuario usuarioObtenido = new Usuario();
 
@@ -52,6 +56,135 @@ public class UsuarioDAOH2 implements UsuarioDAO {
 
 		return usuarioObtenido;
 
+	}
+
+	@Override
+	public Usuario crearUsuario(Usuario usuario) throws DAOException {
+		
+		try {
+			Connection conexion = DBManager.getInstance().getConnection();
+
+			try {
+				
+				/*
+				String sentencia = "INSERT INTO VENTAS(ID_UBICACION, NOMBRE_USUARIO, FECHA_VENTA, NOMBRE_CLIENTE, TELEFONO_CLIENTE, TOTAL) "
+						+ "VALUES (" + venta.getUbicacion().getIdUbicacion() + "', '" + venta.getVendedor().getUsuario()
+						+ "', '" + venta.getFechaVenta() + "', '" + venta.getNombreCliente() + "', '"
+						+ venta.getTelefonoCliente() + "', '" + venta.getTotal() + "');";
+
+				PreparedStatement s = conexion.prepareStatement(sentencia, Statement.RETURN_GENERATED_KEYS);
+				int result = s.executeUpdate();
+				conexion.commit();
+				ResultSet rs = s.getGeneratedKeys();
+
+				if (rs.next()) {
+					venta.setIdVenta(rs.getInt(1));
+				}
+				*/
+				
+			} catch (SQLException e) {
+				conexion.rollback();
+				throw new DAOException();
+			} finally {
+				conexion.close();
+			}
+
+		} catch (SQLException e) {
+			throw new DAOException();
+		}
+
+		return usuario;
+	}
+
+	public Usuario modificarUsuario(Usuario usuario) throws DAOException {
+		
+		try {
+			Connection conexion = DBManager.getInstance().getConnection();
+
+			try {
+				
+				/*
+
+				Statement s = conexion.createStatement();
+
+				String sentencia = "UPDATE LUGAR SET ID_UBICACION = " + venta.getUbicacion().getIdUbicacion() + ", "
+						+ "NOMBRE_CLIENTE = '" + venta.getNombreCliente() + "' ," + "TELEFONO_CLIENTE = '"
+						+ venta.getTelefonoCliente() + "' ," + "TOTAL = '" + venta.getTotal() + "' WHERE ID_VENTA = "
+						+ venta.getIdVenta() + ";";
+
+				s.executeUpdate(sentencia);
+				conexion.commit();
+				*/
+				
+			} catch (SQLException e) {
+				conexion.rollback();
+				throw new DAOException();
+			} finally {
+				conexion.close();
+			}
+
+		} catch (SQLException e) {
+			throw new DAOException();
+		}
+
+		return usuario;
+		
+	}
+
+	public void eliminarUsuario(Usuario usuario) throws DAOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Usuario> listarUsuarios(Usuario usuario) throws DAOException {
+		
+		Connection conexion;
+
+		try {
+			conexion = DBManager.getInstance().getConnection();
+		} catch (SQLException e2) {
+			throw new DAOException();
+		}
+
+		List<Usuario> usuariosObtenidos = new ArrayList<Usuario>();
+
+		String sentencia = "SELECT * FROM USUARIO;";
+
+		try {
+			
+			/*
+			Statement s = conexion.createStatement();
+			ResultSet result = s.executeQuery(sentencia);
+			conexion.commit();
+
+			while (result.next()) {
+				Venta venta = new Venta(
+						result.getInt("ID_VENTA"), 
+						new Espectaculo(result.getInt("ID_ESPECTACULO")), 
+						result.getDate("FECHA_VENTA"),
+						result.getString("NOMBRE_CLIENTE"),
+						result.getInt("TELEFONO_CLIENTE"),
+						result.getDouble("TOTAL"),
+						new Ubicacion(result.getInt("ID_UBICACION")),
+						new Usuario(result.getString("NOMBRE_USUARIO"))
+						);
+
+				ventasObtenidas.add(venta);
+			}
+			*/
+
+		} catch (SQLException e) {
+			throw new DAOException();
+		} finally {
+			try {
+				conexion.close();
+			} catch (SQLException e) {
+				throw new DAOException();
+			}
+		}
+
+		return usuariosObtenidos;
+		
 	}
 
 }
